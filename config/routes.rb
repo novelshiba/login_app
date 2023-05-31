dec156c96930077c9cf5b58880986d98ba0ccf10
novelshiba <taichi.shiba@novel.jpn.com>
2023-05-31 17:53:20 +0900

    Add user session function and post feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users, only: [:show, :edit, :update]
+  resources :lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
